af37a106a4e110cf45e15a4b71c77912b4464862
Saksham Gupta <76028123+sakshgupta@users.noreply.github.com>
2022-03-03 09:27:13 +0530

    Update kohler_scrape.js

--- Kohler/kohler_scrape.js
+++ Kohler/kohler_scrape.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
-let fs = require('fs');
+let xlsx = require('xlsx');
 
 async function getdetails(url, page){
     try{
         await page.goto(url, {
-            waitUntil: "load";
+            waitUntil: "load",
             timeout: 0,
         });
     
@@ -17,8 +17,6 @@ async function getdetails(url, page){
         // Price
         const price = await page.$eval("#koh-page-outer > div > div.koh-page > section > div.koh-product-top-row > div.koh-product-details > div.koh-product-skus-colors > ul > li > span", span => span.textContent);
         
-        let temp1 = price.replace(/,/g, ".");
-    
         // Color Name
         const color = [];
         const img = [];
@@ -27,6 +25,8 @@ async function getdetails(url, page){
         for(let i =0; i<lis.length;i++){
             // Click
             lis[i].click();
+
+            await page.waitForTimeout(1000);
             // Color
             color.push(await page.$eval(".koh-product-colors > span.koh-product-colors-selected", span => span.innerText));
             // Image
@@ -34,30 +34,14 @@ async function getdetails(url, page){
     
             await page.waitForTimeout((Math.floor(Math.random()*3)+1)*1000);
         }
-
-        let temp2 = color.toString();
-        temp2= temp2.replace(/,/g, " ");
-        let temp3 = img.toString();
-        temp3= temp3.replace(/,/g, "    ");
-
-        // To csv
-        if (prod_name !== "Null") {
-            fs.appendFile(
-                "result.csv",
-                `${url}, ${prod_name}, ${code}, ${temp1}, ${temp2}, ${temp3}\n`,
-                function (err) {
-                    if (err) throw err;
-                }
-                );
-        }
     
         return {
             URL: url,
             Name: prod_name,
             Code: code,
-            Price: price,
-            Color_Name: color,
-            Image_Link: img
+            Price: price.toString(),
+            Color_Name: color.toString(),
+            Image_Link: img.toString()
         };
     }
     catch(e){
@@ -67,8 +51,14 @@ async function getdetails(url, page){
 
 async function getLinks(page){
     let links=[];
+    // usage:
+    // -> npm install puppeteer --save
+    // -> just set `url` according to the section that is to be scraped.
+    // -> set the file path at line 93 as the path of the csv to which the scraped data is to be written.
+    // -> run
 
-    await page.goto("https://www.kohler.co.in/browse/Bathroom/Basin", {
+    let url = "Kitchen/Kitchen+Faucets/Product+Type/Wall+Mount";
+    await page.goto("https://www.kohler.co.in/browse/"+url, {
         waitUntil: "load",
         timeout: 0,
     });
@@ -86,6 +76,8 @@ async function main(){
     const alldata = [];
     const allLinks = await getLinks(page);
     let i=0;
+    
+    console.log(allLinks.length);
 
     for(let link of allLinks){
         const data = await getdetails(link,page);
@@ -93,6 +85,13 @@ async function main(){
         // if(i==3) break;
         // i++;
     }
+
+    // To csv
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(alldata);
+    xlsx.utils.book_append_sheet(wb, ws);
+    xlsx.writeFile(wb, "wall_mount.xlsx");
+
     console.log(alldata);
     console.log("Converted to excel file");
     console.log("Done!!");
