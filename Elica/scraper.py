f1470d90af45c7374d6b811835a56211b32adc23
varun <10447555-nairvarun@users.noreply.gitlab.com>
2022-03-13 00:59:37 +0530

    elica: new and improved

--- Elica/scraper.py
+++ Elica/scraper.py
@@ -1,103 +1,100 @@
+import concurrent.futures
 from bs4 import BeautifulSoup
 import requests
 import csv
+import os
 
 
-def scrape(sections, file_names):
-    for i in range(len(sections)):
-        with open(file_names[i], 'w') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(['name', 'url', 'images', 'details', 'additional_features'])
+def scrape(url, use_utf_8):
+    source = requests.get(url)
+    page = BeautifulSoup(source.text, 'lxml')
 
-            print(f'> {sections[i]}')
+    data_dir = './data'
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
 
-            section_source = requests.get(sections[i]).text
-            section_page = BeautifulSoup(section_source, 'lxml')
+    subsection_links = tuple(url + links.attrs['href'] for links in page.select('#lblMenu > nav > div > ul > li > ul > div > li.col-sm-4.no-padding-left.menuleft > div > div > div > ul > li > a'))
+    subsection_files = tuple('./data' + file[len(url):] + '.csv' for file in subsection_links)
+    link_file_iter = tuple(zip(subsection_links, subsection_files))
 
-            pro_box = section_page.find_all('div', class_ = 'pro-box')
-            images = []
-            details = []
-            additional_features = []
-            for item in pro_box:
+    for link, file in link_file_iter:
+        print(f'> {link} --> {file}')
+        if use_utf_8:
+            csv_file = open(file, 'w', newline='', encoding="utf-8")   # otherwise it will throw error for symbols like ₹ on windows
+        else:
+            csv_file = open(file, 'w')
+
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['name', 'url', 'image', 'description'])
+
+        subsection_source = requests.get(link)
+        subsection_page = BeautifulSoup(subsection_source.text, 'lxml')
+
+        p_name = (a.text for a in subsection_page.select('div.pro-box h1 a'))
+        p_url = (url + '/' + a.attrs['href'] for a in subsection_page.select('div.pro-box h1 a'))
+
+        def scrape_product(name_url_iter):
+            print(name_url_iter[1])
+            product_source = requests.get(name_url_iter[1])
+            product_page = BeautifulSoup(product_source.text, 'lxml')
+
+            p_name = name_url_iter[0]
+            p_url = name_url_iter[1]
+
+            try:
+                p_imgs = [url + img.attrs['src'].replace(' ', '%20') for img in product_page.select('ul.list-inline > li > a > img')]
                 try:
-                    name = item.h1.a.text
-                    rel_url = item.h1.a['href']
-                    url = f'https://www.elicaindia.com/{rel_url}'
-                except AttributeError:
-                    continue
-
-                print(url)
-                item_src = requests.get(url).text
-                item_page = BeautifulSoup(item_src, 'lxml')
-
-                item_slider = item_page.find('div', class_ = 'product-slider animated fadeInUp')
-                item_slider_imgs = item_slider.find_all('img', class_ = 'img-responsive')
-                # item_slider_imgs = item_slider_list.find_all('li')
-                for img in item_slider_imgs:
-                    images.append(f'https://www.elicaindia.com{img["src"]}')
-
-                funcs = item_page.find('span', id = 'ContentPlaceHolder1_lblProductVersionHeadingDesc').text
-
-                item_details_box = item_page.find('div', class_ = 'pro-size text-center')
-                item_details = item_details_box.find_all('div')
-                for k in item_details:
-                    details.append(f'{k.h1.span.text} : {k.p.span.text}')
-
-                try:
-                    feat_disc = item_page.find('span', id = "ContentPlaceHolder1_lblProductFeatureDescription")
-                    tbody = feat_disc.find('table')
-                    rows = tbody.find_all('tr')
-                    for tr in rows:
-                        data = tr.find_all('td')
-                        additional_features.append(f'{data[0].text} : {data[1].text}')
-                except AttributeError:
+                    p_imgs.append(url + product_page.select_one('#ContentPlaceHolder1_lblInstallationDiagramImageUrl').attrs['src'].replace(' ', '%20'))
+                except Exception as e:
                     pass
+            except Exception as e:
+                p_imgs = ''
 
-                csv_writer.writerow([name, url, images, details, additional_features])
+            try:
+                p_info1 = [
+                    product_page.select_one('div.col-sm-6.diagram-text.text-center :nth-child(1)').text.replace('\n', '') + ': ' +
+                    product_page.select_one('div.col-sm-6.diagram-text.text-center :nth-child(2)').text.replace('\n', ''),
+                    product_page.select_one('div.col-sm-6.diagram-text.text-center :nth-child(3)').text.replace('\n', '') + ': ' +
+                    product_page.select_one('div.col-sm-6.diagram-text.text-center :nth-child(4)').text.replace('\n', '')
+                ]
+            except Exception as e:
+                p_info1 = []
+            try:
+                p_info2_zip = zip(
+                    [span.text for span in product_page.select('#form1 > div:nth-child(13) > div > div > h1 > span')],
+                    [span.text for span in product_page.select('#form1 > div:nth-child(13) > div > div > p > span')]
+                )
+                p_info2 = [i[0]+': '+i[1] for i in p_info2_zip]
+            except Exception as e:
+                p_info2 = []
+            try:
+                p_info3 = [
+                    product_page.select_one('div.pro-text > div > div > h1 > span').text + ': ' +
+                    product_page.select_one('div.pro-text > div > div > p > span').text
+                ]
+            except Exception as e:
+                p_info3 = []
+            p_info = p_info1 + p_info2 + p_info3
 
+            csv_writer.writerow([p_name, p_url, p_imgs, p_info])
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(scrape_product, zip(p_name, p_url))
+
+        csv_file.close()
 
 
 if __name__ == '__main__':
     '''
     usage:
     -> install requirements
-    -> verify:
-        - all site sections are listed in `sections`
-        - all sites have a corresponding csv file `file_names`
-        - all paths to the csv file are correct
+    -> check if url is correct
     -> run
     '''
 
-    sections = [
-        'https://www.elicaindia.com/Chimneys',
-        'https://www.elicaindia.com/Built_In_Induction_Hobs',
-        'https://www.elicaindia.com/Built-In_Combo_Oven',
-        'https://www.elicaindia.com/Built-In_Hobs',
-        'https://www.elicaindia.com/Built-In_Ovens',
-        'https://www.elicaindia.com/Built-In_Microwave_Ovens',
-        'https://www.elicaindia.com/Barbeques',
-        'https://www.elicaindia.com/Dishwashers',
-        'https://www.elicaindia.com/Fryers',
-        'https://www.elicaindia.com/Warmer_Drawer',
-        'https://www.elicaindia.com/WineCooler',
-        'https://www.elicaindia.com/Cooktops',
-        'https://www.elicaindia.com/Cooking_Ranges'
-    ]
+    if os.name == 'posix':
+        use_utf_8 = False
+    else:
+        use_utf_8 = True
 
-    file_names = [
-        './data/Chimneys.csv',
-        './data/Built_In_Induction_Hobs.csv',
-        './data/Built-In_Combo_Oven.csv',
-        './data/Built-In_Hobs.csv',
-        './data/Built-In_Ovens.csv',
-        './data/Built-In_Microwave_Ovens.csv',
-        './data/Barbeques.csv',
-        './data/Dishwashers.csv',
-        './data/Fryers.csv',
-        './data/Warmer_Drawer.csv',
-        './data/WineCooler.csv',
-        './data/Cooktops.csv',
-        './data/Cooking_Ranges.csv'
-    ]
-
-    scrape(sections, file_names)
+    scrape('https://www.elicaindia.com', use_utf_8)
